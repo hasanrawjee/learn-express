d8370f1630d1c4971bb37dfa895538400bf8285b
Hasan Rawjee <105398848+hasanrawjee@users.noreply.github.com>
2024-03-15 23:54:26 -0400

    Todos

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,16 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+    let usernames = req.users.map(function (user) {
+        return { id: user.id, username: user.username };
+    });
+    res.send(usernames);
+});
+
+router.get('/username/:name', (req, res) => {
+    let user = req.users.filter((user) => user.username === req.params.name)[0];
+    res.send([{ id: user.id, email: user.email }]);
+});
+
+module.exports = router;
